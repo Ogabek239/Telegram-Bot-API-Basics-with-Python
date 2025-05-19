417614b2b02eb604e7d98de83c33af5cd97517d0
Ogabek239 <ogabekzopirov16@gmail.com>
2025-05-19 14:15:05 +0500

    bajardim

--- message_handler.py
+++ message_handler.py
@@ -1,7 +1,30 @@
+import requests
+from config import BASE_URL
 def handle_message(message):
     """
     Optional Challenge: Conditional Responses
     - Handle different message inputs
     - Return appropriate responses
     """
-    pass
+    r = requests.get(BASE_USL + '/getMessage')
+
+    data = r.json()
+    songi_qadam = data['message']['text']
+
+    chat_id = songi_qadam['message']['chat']['id']
+    text = songi_qadam['massage']['text']
+
+    javob = 'men tushunmadim'
+    if text == 'hi':
+        javob = 'hello'
+    elif text == 'bye':
+        javob = 'goodbye'
+
+
+    requests.post(BASE_URL + '/sendMessage', params={
+        'chat_id': chat_id,
+        'text': javob
+    })
+
+
+handle_message()
